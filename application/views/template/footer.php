b88a70aceb62636cf0c4c13a735b12605133fb72
Kalpesh <kalpesh.khandla@bosctechlabs.com>
2021-05-07 18:15:54 +0530

    Curd

---
+++ application/views/template/footer.php
@@ -0,0 +1,10 @@
+</body>
+
+<script src="<?php echo base_url();?>/assets/js/pagination.js"></script>
+<script src="<?php echo base_url();?>/assets/js/app.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		  
+	})
+</script>
+</html>
